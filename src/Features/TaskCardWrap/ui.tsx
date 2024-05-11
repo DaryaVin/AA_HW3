24e1d2593cb03a105aad97becc2e7435d09283d9
Eblaraa <baraax01@gmail.com>
2024-05-11 11:56:11 +0700

    commit baraa

--- src/Features/TaskCardWrap/ui.tsx
+++ src/Features/TaskCardWrap/ui.tsx
@@ -1,58 +1,55 @@
 import React, { useState } from "react";
-// import ColumnCard from "../../Entities/Column/UI/ColumnCard/ui";
 import { Modal } from "../../Shared/UI/Modal/ui";
-// import {ColumnItem} from "../../Entities/Column";
-// import { useAppSelector } from "../../Shared";
-// import { UpdateColumnForm } from "../../Entities/Column";
 import { UpdateTaskForm } from "../../Entities/Task";
 import { TaskItem } from "../../Entities/Task/Model/type";
 import TaskCard from "../../Entities/Task/Model/Ui/TaskCard/ui";
+import { deleteTaskActionCreator } from "../../Entities/Task/Store/actionCreators";
+import { useAppDispatch } from "../../Shared";
 
 interface TaskCardWrapProps {
-  TaskItem: TaskItem;
-  color: {
-    r: number;
-    g: number;
-    b: number;
-  };
+    TaskItem: TaskItem;
+    color: {
+        r: number;
+        g: number;
+        b: number;
+    };
 }
 export const TaskCardWrap = ({ TaskItem, color }: TaskCardWrapProps) => {
-  // const dispatch = useAppDispatch();
-  //
+    const dispatch = useAppDispatch();
   // const ColumnsState = useAppSelector((state) => state.ColumnsReducer);
 
-  const [ShowUpdateTask, setShowUpdateTask] = useState<boolean>(false);
+    const [ShowUpdateTask, setShowUpdateTask] = useState<boolean>(false);
 
-  return (
-    <>
-      <TaskCard
-        name={TaskItem.name}
-        id={TaskItem.id}
-        description={TaskItem.description}
-        status={TaskItem.status}
-        tags={TaskItem.tags}
-        color={color}
-        editFun={() => {
-          setShowUpdateTask(true);
-        }}
-        deleteFun={() => {
-          console.log("dfsfs");
-        }}
-      />
+    return (
+        <>
+        <TaskCard
+            name={TaskItem.name}
+            id={TaskItem.id}
+            description={TaskItem.description}
+            status={TaskItem.status}
+            tags={TaskItem.tags}
+            color={color}
+            editFun={() => {
+            setShowUpdateTask(true);
+            }}
+            deleteFun={() => {
+            dispatch(deleteTaskActionCreator(TaskItem))
+            }}
+        />
 
-      <Modal setIsShow={setShowUpdateTask} isShow={ShowUpdateTask}>
-        {ShowUpdateTask && (
-          <UpdateTaskForm
-            taskItem={TaskItem}
-            onClickSaveBtn={() => {
-              setShowUpdateTask(false);
-            }}
-            onClickCanselBtn={() => {
-              setShowUpdateTask(false);
-            }}
-          />
-        )}
-      </Modal>
-    </>
-  );
+        <Modal setIsShow={setShowUpdateTask} isShow={ShowUpdateTask}>
+            {ShowUpdateTask && (
+            <UpdateTaskForm
+                taskItem={TaskItem}
+                onClickSaveBtn={() => {
+                setShowUpdateTask(false);
+                }}
+                onClickCanselBtn={() => {
+                setShowUpdateTask(false);
+                }}
+            />
+            )}
+        </Modal>
+        </>
+    );
 };
